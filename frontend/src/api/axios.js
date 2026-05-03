dd578e8ceb767c6dfdd964f3492899bedbee4736
yadavsriyansh400-ux <yadavsriyansh400@gmail.com>
2026-05-03 14:27:55 +0530

    update API baseURL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://blog-platform-9xw9.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
