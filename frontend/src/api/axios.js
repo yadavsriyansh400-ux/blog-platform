ff6296dde21ec9d9715af288061cd103405bc1a5
yadavsriyansh400-ux <yadavsriyansh400@gmail.com>
2026-05-03 15:17:23 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blog-platform-9xw9.onrender.com",
+  baseURL: "https://blog-platform-9xw9.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
